a11478eabd757dff2049ebb0e3b035dcd0360366
Bilal Barzinji <bilalbarzinji@gmail.com>
2014-12-29 16:44:47 +0300

    Desgin Improvemnts 0.1
    BootStrap 3 form . 
    Clean HTML / CSS 
    Github Frok link to me added .

--- host_input.php
+++ host_input.php
@@ -1,5 +1,53 @@
-<form action="index.php" method="post">
-    Ip/link:  <input type="text" name="host" /><br />
-	
-    <input type="submit" name="submit" value="Submit me!" />
+<!DOCTYPE html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+
+<head> 
+<div class="container">
+
+
+
+<p class="bg-primary">
+This is Created For Learning Purpose and can be used Freely By anyone .
+</p>
+<form class="form-horizontal" action="index.php" method="post" >
+<fieldset>
+<div class="form-group">
+  <label class="col-md-4 control-label" for="host">Host / IP</label>  
+  <div class="col-md-4">
+  <input id="host" name="host" placeholder="www.b2l2l.com" class="form-control input-md" required="" type="text">
+    
+  </div>
+</div>
+<div class="form-group">
+  <label class="col-md-4 control-label" for="checks">Check Select</label>
+  <div class="col-md-4">
+    <select id="checks" name="checks" class="form-control">
+      <option value="80">up</option>
+      <option value="20">ftp</option>
+    </select>
+  </div>
+</div>
+<div class="form-group">
+  <label class="col-md-4 control-label" for="arsl"></label>
+  <div class="col-md-4">
+    <button id="arsl" name="arsl" class="btn btn-success">click me!</button>
+  </div>
+</div>
+</div>
+</fieldset>
 </form>
+
+
+
+<address><center>
+  <strong>Fork Me on </strong><br>
+  <a href="https://github.com/b2l2l/advance_port_checker/fork">Github & Star <3 </a>
+</address></center>
+
+
+</head> 
+</html> 
